bb30890d857d427101e0fee7b758ff9f48caad58
freynw <freynw@hotmail.com>
2018-02-25 14:55:22 +0300

    app finised

---
+++ app/src/main/java/com/example/ar430878/interactivestory/model/Choice.java
@@ -0,0 +1,31 @@
+package com.example.ar430878.interactivestory.model;
+
+/**
+ * Created by ilhan on 24.02.2018.
+ */
+
+public class Choice {
+    private int textId;
+    private int nextPage;
+
+    public Choice(int textId, int nextPage) {
+        this.textId = textId;
+        this.nextPage = nextPage;
+    }
+
+    public int getTextId() {
+        return textId;
+    }
+
+    public void setTextId(int textId) {
+        this.textId = textId;
+    }
+
+    public int getNextPage() {
+        return nextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        this.nextPage = nextPage;
+    }
+}
